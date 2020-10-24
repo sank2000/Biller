7831b8a92aa4ac5eb7e957e3e0cf8bbce6da1c35
sank2000 <santhoshvelr@yahoo.com>
2020-10-24 14:58:06 +0530

    home pade added

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import { AppBar } from '../../components';
+import { Typography } from '@material-ui/core';
+import classes from "./Home.module.scss";
+
+function Card({ name, image, link, ...props }) {
+  return (
+    <a href={link} className="col-1-of-3" style={{ textDecoration: "none" }}>
+      <div
+        className={classes.card}
+        style={{
+          backgroundImage: `linear-gradient(to right,rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${image})`,
+        }}
+        {...props}
+      >
+        <Typography variant="h5" className={classes.text}>{name}</Typography>
+      </div>
+    </a>
+  );
+}
 
 export default function Home() {
-  return <AppBar />;
+  return <>
+    <AppBar />
+    <section className={classes.section}>
+      <Card name="Pay Bill" image='/images/pay.jpg' link="#a" />
+      <Card name="Create Bill" image='/images/create.jpg' link="#a" />
+      <Card name="Transaction History" image='/images/history.jpg' link="#a" />
+      <Card name="Create Account" image='/images/create.jpg' link="#a" />
+    </section>
+  </>;
 }
